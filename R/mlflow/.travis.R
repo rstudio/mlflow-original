89a8f52285cbeb10db68130c7c0f549e9df9e1b6
Javier Luraschi <javierluraschi@hotmail.com>
2018-08-24 09:02:15 -0700

    fix path to testthat source

--- R/mlflow/.travis.R
+++ R/mlflow/.travis.R
@@ -2,4 +2,4 @@ parent_dir <- dir(full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package, repos = NULL, type = "source")
 
-source("tests/testthat.R")
+source("R/mlflow/tests/testthat.R")
